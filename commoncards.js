8940a0397eeaa15fe01df5de44a9099d78983fc3
voxrox <49262311+voxrox@users.noreply.github.com>
2021-02-16 10:35:41 +0530

    Update commoncards.js

--- commoncards.js
+++ commoncards.js
@@ -112,7 +112,7 @@ class Commoncards1{
     {
         const card = CardFactory.heroCard(
             'Please select any option to proceed further',
-           
+            ['https://wallpaperaccess.com/full/1556608.jpg'],
             ['Create Claim','Get Claim Details']
        );
        const message = MessageFactory.attachment(card);
