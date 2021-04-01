99295b14a01544f95fe2cf3473ee9a702ac4cc25
voxrox <49262311+voxrox@users.noreply.github.com>
2021-04-01 12:43:12 +0530

    Update servicenow.js

--- dialogs/servicenow/servicenow.js
+++ dialogs/servicenow/servicenow.js
@@ -67,7 +67,7 @@ class Servicenow extends ComponentDialog{
         var usernamepassword1="Basic YWRtaW46VXhMcEkwdWd4R0Yx"
         var incident=step.result
         
-        var url="https://dev61713.service-now.com/api/now/table/incident?sysparm_query=number%3D"+incident+"&sysparm_limit=1"
+        var url="https://dev92108.service-now.com/api/now/table/incident?sysparm_query=number%3D"+incident+"&sysparm_limit=1"
         var contentType='application/json'
         var loginstance = axios.create();
         loginstance.defaults.headers.common['Content-Type']=contentType
